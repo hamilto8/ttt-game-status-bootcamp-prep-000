4a4d390e6b8a243323fe8b88aef9c73167351b36
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-08 13:31:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,20 +25,19 @@ def won?(board)
       position_3 = board[win_index_3]
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-      arr = [win_index_1, win_index_2, win_index_3]
-      return arr
+        return win_combination
 
     elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      arr = [win_index_1, win_index_2, win_index_3]
-      return arr
-    elsif (board[2] == "X" && board[4] == "X" && board [6] == "X")
-        arr = [2, 4, 6]
-        return arr
-    else
+        return win_combination
+
+    elsif (board == [" ", " ", " ", " ", " ", " ", " ", " ", " "])
           return false
+    elsif (full?(board))
+          false
     end
   end
 end
+
 
 # def full?(board)
 #   count = 0
